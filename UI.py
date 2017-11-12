2994b0f9c59b8c4dbc30e70470c2ea2da6ab9ffa
Atech1 <myself.therest@gmail.com>
2017-11-12 04:34:02 -0500

    Adding needed UI Module

---
+++ UI.py
@@ -0,0 +1,16 @@
+# Alexander Ross (asr3bj), UI.py
+# this was created Nov, 2017
+
+class UI_Element:  # from_text()
+
+    def __init__(self, x, y, width, textobject):
+
+
+def TextDescriptor(font='arial', text="", size='6', bold=False, italic=False):
+    instance_TextDescriptor = dict()
+    instance_TextDescriptor['font'] = font
+    instance_TextDescriptor['text'] = text
+    instance_TextDescriptor['size'] = size
+    instance_TextDescriptor['bold'] = bold
+    instance_TextDescriptor['italic'] = italic
+    return instance_TextDescriptor
--- game.py
+++ game.py
@@ -1,2 +1,9 @@
 # Alexander Ross (asr3bj) and Tilden (), program.py
 # this was created Nov, 2017
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
